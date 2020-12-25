4af353147e03430d06776dd0b9c7d6a528a45d82
benny <benny08118@gmail.com>
2020-12-25 23:42:45 +0800

    TDD about Tag add 4 feature ; blank , name only , name can't too long and too short

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 3, maximum: 25} 
+  validates :name, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -7,13 +7,35 @@ RSpec.describe Tag, type: :model do
         tag =FactoryBot.build(:tag)
         expect(tag).to be_valid
       end
-      # it '標籤名字不能空白',tags:true do
-      #   tag=FactoryBot.build(:tag,name:'')
-      #   except{
-      #          except(tag).to be_valid
-      #   }.to raise_exception(/Name 不能空白/)
+      it '標籤名字不能空白',tags: true do
+        tag=FactoryBot.build(:tag,name: '')
+        expect{
+                expect(tag).to be_valid
+              }.to raise_exception(/Name can't be blank/)
         
-      # end
+      end
+      it '標籤名字必須唯一',tags: true do
+        tag1=FactoryBot.create(:tag,name: 'box')
+        tag2=FactoryBot.build(:tag,name: 'box')
+        expect{
+                expect(tag2).to be_valid
+              }.to raise_exception(/Name has already been taken/)
+        
+      end
+      it '標籤名字不能太長',tags: true do
+        tag=FactoryBot.build(:tag,name: 'k'*26)
+        expect{
+                expect(tag).to be_valid
+              }.to raise_exception(/Name is too long/)
+        
+      end
+      it '標籤名字不能太短',tags: true do
+        tag=FactoryBot.build(:tag,name: 'k'*2)
+        expect{
+                expect(tag).to be_valid
+              }.to raise_exception(/Name is too short/)
+        
+      end
     end
   end
 end
